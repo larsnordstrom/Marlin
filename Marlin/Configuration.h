477af7338fe0d0bf9ed40599fb502fcc088b728f
larsnordstrom <larsnordstrom@me.com>
2020-02-03 14:11:59 +0100

    Changed Z-offset to 0

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -956,7 +956,7 @@
  */
 #define NOZZLE_TO_PROBE_OFFSET \
   {                            \
-    23, 5, -0.795              \
+    23, 5, 0              \
   }
 
 // Most probes should stay away from the edges of the bed, but
