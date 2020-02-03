bc463dddbfab80575daf3ed406bab93771947bf1
lano78 <larsnordstrom@me.com>
2020-02-03 22:05:45 +0100

    Set homing currents

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2052,16 +2052,16 @@
 #define INTERPOLATE true    // Interpolate X/Y/Z_MICROSTEPS to 256
 
 #if AXIS_IS_TMC(X)
-#define X_CURRENT 430                  // (mA) RMS current. Multiply by 1.414 for peak current.
-#define X_CURRENT_HOME (X_CURRENT / 2) // (mA) RMS current for sensorless homing
-#define X_MICROSTEPS 16                // 0..256
+#define X_CURRENT 394      // (mA) RMS current. Multiply by 1.414 for peak current.
+#define X_CURRENT_HOME 149 // (mA) RMS current for sensorless homing
+#define X_MICROSTEPS 16    // 0..256
 #define X_RSENSE 0.11
 #define X_CHAIN_POS -1 // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
 #endif
 
 #if AXIS_IS_TMC(Y)
-#define Y_CURRENT 480
-#define Y_CURRENT_HOME (Y_CURRENT / 2)
+#define Y_CURRENT 487
+#define Y_CURRENT_HOME 182
 #define Y_MICROSTEPS 16
 #define Y_RSENSE 0.11
 #define Y_CHAIN_POS -1
@@ -2069,7 +2069,7 @@
 
 #if AXIS_IS_TMC(Z)
 #define Z_CURRENT 580
-#define Z_CURRENT_HOME (Z_CURRENT / 2)
+#define Z_CURRENT_HOME 348
 #define Z_MICROSTEPS 16
 #define Z_RSENSE 0.11
 #define Z_CHAIN_POS -1
@@ -2077,14 +2077,14 @@
 
 #if AXIS_IS_TMC(Z2)
 #define Z2_CURRENT 580
-#define Z2_CURRENT_HOME (Z2_CURRENT / 2)
+#define Z2_CURRENT_HOME 348
 #define Z2_MICROSTEPS 16
 #define Z2_RSENSE 0.11
 #define Z2_CHAIN_POS -1
 #endif
 
 #if AXIS_IS_TMC(E0)
-#define E0_CURRENT 550
+#define E0_CURRENT 514
 #define E0_MICROSTEPS 16
 #define E0_RSENSE 0.11
 #define E0_CHAIN_POS -1
