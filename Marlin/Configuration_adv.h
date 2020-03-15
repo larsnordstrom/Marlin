0309c3c592b2093446b7acaa6796be0bcd9d5ae6
lano78 <larsnordstrom@me.com>
2020-03-15 19:38:36 +0100

    Increased watch period

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -169,7 +169,7 @@
    * and/or decrease WATCH_TEMP_INCREASE. WATCH_TEMP_INCREASE should not be set
    * below 2.
    */
-#define WATCH_TEMP_PERIOD 20  // Seconds
+#define WATCH_TEMP_PERIOD 120 // Seconds
 #define WATCH_TEMP_INCREASE 8 // Degrees Celsius
 #endif
 
