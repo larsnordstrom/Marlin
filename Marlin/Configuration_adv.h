812c9193bab8ef95ac78d04ebb5cd5aacdd35a0d
larsnordstrom <larsnordstrom@me.com>
2020-03-22 14:03:59 +0100

    Lowered homing currents.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2074,7 +2074,7 @@
 
 #if AXIS_IS_TMC(X)
 #define X_CURRENT 500      // (mA) RMS current. Multiply by 1.414 for peak current.
-#define X_CURRENT_HOME 200 // (mA) RMS current for sensorless homing
+#define X_CURRENT_HOME 180 // (mA) RMS current for sensorless homing
 #define X_MICROSTEPS 16    // 0..256
 #define X_RSENSE 0.11
 #define X_CHAIN_POS -1 // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
@@ -2082,7 +2082,7 @@
 
 #if AXIS_IS_TMC(Y)
 #define Y_CURRENT 600
-#define Y_CURRENT_HOME 240
+#define Y_CURRENT_HOME 200
 #define Y_MICROSTEPS 16
 #define Y_RSENSE 0.11
 #define Y_CHAIN_POS -1
@@ -2105,7 +2105,7 @@
 #endif
 
 #if AXIS_IS_TMC(E0)
-#define E0_CURRENT 550
+#define E0_CURRENT 520
 #define E0_MICROSTEPS 16
 #define E0_RSENSE 0.11
 #define E0_CHAIN_POS -1
@@ -2201,7 +2201,7 @@
    * Define you own with
    * { <off_time[1..15]>, <hysteresis_end[-3..12]>, hysteresis_start[1..8] }
    */
-#define CHOPPER_TIMING CHOPPER_DEFAULT_24V
+#define CHOPPER_TIMING CHOPPER_PRUSAMK3_24V
 
 /**
    * Monitor Trinamic drivers for error conditions,
