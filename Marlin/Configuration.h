ae92e3aa9d0888d7e9ba8d0959fc97aa59a313b2
larsnordstrom <larsnordstrom@me.com>
2020-03-25 10:43:56 +0100

    Lowered homing feedrate to 45mms

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1377,7 +1377,7 @@
 #endif
 
 // Homing speeds (mm/m)
-#define HOMING_FEEDRATE_XY (3000)
+#define HOMING_FEEDRATE_XY (2700)
 #define HOMING_FEEDRATE_Z (480)
 
 // Validate that endstops are triggered on homing moves
