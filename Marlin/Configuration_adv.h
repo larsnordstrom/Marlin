e0ae249e9e7dc20ba919cce9655b3100e4a48d1f
Lars Nordström <larsnordstrom@me.com>
2020-04-11 15:13:51 +0200

    increased currents

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2085,7 +2085,7 @@
 #define INTERPOLATE true    // Interpolate X/Y/Z_MICROSTEPS to 256
 
 #if AXIS_IS_TMC(X)
-#define X_CURRENT 550      // (mA) RMS current. Multiply by 1.414 for peak current.
+#define X_CURRENT 600      // (mA) RMS current. Multiply by 1.414 for peak current.
 #define X_CURRENT_HOME 180 // (mA) RMS current for sensorless homing
 #define X_MICROSTEPS 16    // 0..256
 #define X_RSENSE 0.11
@@ -2117,7 +2117,7 @@
 #endif
 
 #if AXIS_IS_TMC(E0)
-#define E0_CURRENT 520
+#define E0_CURRENT 540
 #define E0_MICROSTEPS 16
 #define E0_RSENSE 0.11
 #define E0_CHAIN_POS -1
