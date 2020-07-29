14a02536be9caf1c6cf8b19c9c59faf9e6ca7005
Lars Nordström <larsnordstrom@me.com>
2020-07-29 14:20:03 +0200

    Config tweaks

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -453,7 +453,7 @@
  * the fan will turn on when any selected extruder is above the threshold.
  */
 //#define FAN1_PIN P1_27 // X-Stop
-#define E0_AUTO_FAN_PIN P2_04 // P1_28 // Y-Stop
+#define E0_AUTO_FAN_PIN P2_04 //FAN1_PIN
 #define E1_AUTO_FAN_PIN -1
 #define E2_AUTO_FAN_PIN -1
 #define E3_AUTO_FAN_PIN -1
