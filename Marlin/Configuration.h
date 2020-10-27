a540c286aa26a39edcd8a15d789116757a66d8eb
Lars Nordström <lars.nordstrom@jgy.se>
2020-10-27 21:00:34 +0100

    Switchwire motionsystem changed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -131,7 +131,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "BabaBear TURBO"
+#define CUSTOM_MACHINE_NAME "Switchwire"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
@@ -613,7 +613,7 @@
 // Enable one of the options below for CoreXY, CoreXZ, or CoreYZ kinematics,
 // either in the usual order or reversed
 //#define COREXY
-//#define COREXZ
+#define COREXZ
 //#define COREYZ
 //#define COREYX
 //#define COREZX
