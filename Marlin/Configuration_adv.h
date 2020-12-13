7c641cb7c10976e6fd870eda3625f741efad56c3
Lars Nordström <lars.nordstrom@jgy.se>
2020-12-13 18:57:32 +0100

    Z current increase

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2319,7 +2319,7 @@
 #endif
 
 #if AXIS_IS_TMC(Z)
-#define Z_CURRENT 550
+#define Z_CURRENT 620
 #define Z_CURRENT_HOME Z_CURRENT / 2
 #define Z_MICROSTEPS 16
 #define Z_RSENSE 0.11
@@ -2454,15 +2454,15 @@
    */
   #define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     100  // [mm/s]
+  #define X_HYBRID_THRESHOLD     70  // [mm/s]
   #define X2_HYBRID_THRESHOLD    120
-  #define Y_HYBRID_THRESHOLD     100
+  #define Y_HYBRID_THRESHOLD     70
   #define Y2_HYBRID_THRESHOLD    120
-  #define Z_HYBRID_THRESHOLD       3
+  #define Z_HYBRID_THRESHOLD       5
   #define Z2_HYBRID_THRESHOLD      5
   #define Z3_HYBRID_THRESHOLD      3
   #define Z4_HYBRID_THRESHOLD      3
-  #define E0_HYBRID_THRESHOLD     30
+  #define E0_HYBRID_THRESHOLD     45
   #define E1_HYBRID_THRESHOLD     30
   #define E2_HYBRID_THRESHOLD     30
   #define E3_HYBRID_THRESHOLD     30
